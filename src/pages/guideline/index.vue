dbe4636da83c9c87e791f20cfc638e09c12fa73f
sylvadoc <legacjf@gmail.com>
2022-02-08 14:34:04 +0100

    add stylesheets

--- src/pages/guideline/index.vue
+++ src/pages/guideline/index.vue
@@ -35,26 +35,5 @@
 </template>
 
 <style lang="scss">
-@import '../../styles/01-settings/variables';
-.colors {
-    border-collapse: collapse;
-    width: 40rem;
-    th, td {
-        padding: .5rem;
-        text-align: left;
-    }
-}
-.container-global {
-    max-width: 100rem;
-    margin: 0 auto;
-}
-.container {
-    display: grid;
-    grid-template-columns: 5rem 1fr 1fr;
-    grid-column-gap: 2rem;
-
-    h2 {
-        grid-column: 1/4;
-    }
-}
+@import '../../styles/02-layouts/guideline';
 </style>
