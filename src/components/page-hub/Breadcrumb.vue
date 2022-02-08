f8f49f635ec8f71d951b95104297785d8e0153e1
sylvadoc <legacjf@gmail.com>
2022-02-08 15:45:29 +0100

    divers fix

---
+++ src/components/page-hub/Breadcrumb.vue
@@ -0,0 +1,19 @@
+<template>
+    <nav class="breadcrumb-nav" aria-label="Fil d'Ariane des pages immobilier">
+        <ol>
+            <li>
+                <router-link to="/">
+                    Accueil
+                </router-link>
+            </li>
+            <li>
+                <router-link to="/immobilier">
+                    Immobilier
+                </router-link>
+            </li>
+            <li aria-current="location">
+                page courante
+            </li>
+        </ol>
+    </nav>
+</template>
